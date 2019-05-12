625703882db54baa46cbaed24758ce4a2b0d3081
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-05-12 15:02:44 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# bura.to
+
+My personal landing page. Based on Identity by HTML5 UP
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/3f558221-8772-4a91-9d09-194c98fcb932/deploy-status)](https://app.netlify.com/sites/burato/deploys)
---
+++ config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteTitle: 'Alessandro Burato', // <title>
+  manifestName: 'aleburato',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'Alessandro Burato',
+  heading: 'Sr. Software Engineer',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-linkedin-square',
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/alessandro-burato-462a80201',
+    },
+    {
+      icon: 'fa-github-square',
+      name: 'Github',
+      url: 'https://github.com/aleburato',
+    },
+    {
+      icon: 'fa-twitter-square',
+      name: 'Twitter',
+      url: 'https://twitter.com/ale_burato',
+    },
+    // {
+    //   icon: 'fa-facebook-square',
+    //   name: 'Facebook',
+    //   url: 'https://www.facebook.com/aleburato',
+    // },
+    {
+      icon: 'fa-inbox',
+      name: 'Email',
+      url: 'mailto:ale@bura.to',
+    },
+  ],
+};
