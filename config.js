da76fd9f8f2f0c3e07f43cd00d4fb6d7ed82780a
Alessandro Burato <alessandro.burato@outlook.com>
2020-12-17 10:56:11 +0100

    Tweak config

--- config.js
+++ config.js
@@ -1,31 +1,31 @@
 module.exports = {
-  siteTitle: 'Alessandro Burato', // <title>
-  manifestName: 'aleburato',
-  manifestShortName: 'Landing', // max 12 characters
-  manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
-  manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
+  siteTitle: "Alessandro Burato", // <title>
+  manifestName: "aleburato",
+  manifestShortName: "Bura.to", // max 12 characters
+  manifestStartUrl: "/",
+  manifestBackgroundColor: "#663399",
+  manifestThemeColor: "#663399",
+  manifestDisplay: "standalone",
+  manifestIcon: "src/assets/img/website-icon.png",
   pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
-  authorName: 'Alessandro Burato',
-  heading: 'Sr. Software Engineer',
+  authorName: "Alessandro Burato",
+  heading: "Sr. Software Engineer",
   // social
   socialLinks: [
     {
-      icon: 'fa-linkedin-square',
-      name: 'LinkedIn',
-      url: 'https://www.linkedin.com/in/alessandro-burato-462a80201',
+      icon: "fa-linkedin",
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/alessandro-burato-462a80201",
     },
     {
-      icon: 'fa-github-square',
-      name: 'Github',
-      url: 'https://github.com/aleburato',
+      icon: "fa-github-alt",
+      name: "Github",
+      url: "https://github.com/aleburato",
     },
     {
-      icon: 'fa-twitter-square',
-      name: 'Twitter',
-      url: 'https://twitter.com/ale_burato',
+      icon: "fa-twitter",
+      name: "Twitter",
+      url: "https://twitter.com/ale_burato",
     },
     // {
     //   icon: 'fa-facebook-square',
@@ -33,9 +33,9 @@ module.exports = {
     //   url: 'https://www.facebook.com/aleburato',
     // },
     {
-      icon: 'fa-inbox',
-      name: 'Email',
-      url: 'mailto:ale@bura.to',
+      icon: "fa-paper-plane",
+      name: "Email",
+      url: "mailto:ale@bura.to",
     },
   ],
 };
